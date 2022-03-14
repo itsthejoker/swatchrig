eedf255e50733d30ead8aae1d4839fe58e135564
Joe Kaufeld <opensource@joekaufeld.com>
2022-03-14 16:28:20 -0400

    update deps

--- extras/build.sh
+++ extras/build.sh
@@ -2,7 +2,7 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install make build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
 libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
-sudo apt install xterm unclutter -y
+sudo apt install xterm unclutter imagemagick -y
 
 curl https://pyenv.run | bash
 
